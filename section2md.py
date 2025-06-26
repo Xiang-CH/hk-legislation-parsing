a4a0a48df6f1998cfdc760fee8b5e90c3d1859bc
Xiang <cxiang@connect.hku.hk>
2025-06-26 19:32:03 +0800

    fix custom parser

--- section2md.py
+++ section2md.py
@@ -8,6 +8,33 @@ def normalize_whitespace(text):
     """
     return re.sub(r'\s{2,}', ' ', text)
 
+
+def getContentBefore(para, name):
+        content_before = ""
+        for child in para.children:
+            if isinstance(child, bs4.Tag) and child.name == name:
+                break
+            child_content = normalize_whitespace(child.get_text(separator=" ", strip=True)).strip()
+            if child_content: 
+                content_before += child_content + " "
+        return content_before.strip()
+
+def parseInnerParagraph(para):
+    paragraph_text = ""
+    content_before = getContentBefore(para, "paragraph")
+    for para in para.find_all("paragraph"):
+        if para.find("subparagraph"):
+            paragraph_text += f"\t{getContentBefore(para, "subparagraph")}\n"
+            for subpara in para.find_all("subparagraph"):
+                subpara_text = f"\t\t{normalize_whitespace(subpara.get_text(separator=' ', strip=True))}\n"
+                paragraph_text += subpara_text
+            continue
+
+        paragraph_text += f"\t{normalize_whitespace(para.get_text(separator=' ', strip=True))}\n"
+
+    return content_before, paragraph_text
+
+
 def section2md(section):
     text = ""
     heading_no = None
@@ -17,7 +44,10 @@ def section2md(section):
         note.decompose()
 
     for i, tag in enumerate(tags):
-        if tag.name == "heading":
+        # print(tag)
+        if isinstance(tag, bs4.NavigableString):
+            text += normalize_whitespace(tag.strip())
+        elif tag.name == "heading":
             if heading_no:
                 text += f"## {heading_no} {normalize_whitespace(tag.get_text(separator=" ", strip=True))}\n\n"
                 heading_no = None
@@ -35,44 +65,309 @@ def section2md(section):
 
         elif tag.name == "subsection":
             if tag.find("paragraph"):
-                # Append text from all elements preceding the first paragraph
-                content_before = ""
-                for child in tag.children:
-                    if isinstance(child, bs4.Tag) and child.name == "paragraph":
-                        break
-                    child_content = normalize_whitespace(child.get_text(separator=" ", strip=True)).strip()
-                    if child_content: 
-                        content_before += child_content + " "
+                content_before, paragraph_text = parseInnerParagraph(tag)
 
-                text += f"### {content_before}\n"
-
-                for para in tag.find_all("paragraph"):
-                    text += f"\t{normalize_whitespace(para.get_text(separator=' ', strip=True))}\n"
-
-                text += "\n"
+                if content_before:
+                    text += f"### {content_before}\n{paragraph_text}\n"
+                else:
+                    text += f"{paragraph_text}\n"
                 continue
 
             text += f"### {normalize_whitespace(tag.get_text(separator=' ', strip=True))}\n\n"
+
+        elif tag.name == "paragraph":
+            if tag.find("subparagraph"):
+                text += f"\t{getContentBefore(tag, "subparagraph")}\n"
+                for subpara in tag.find_all("subparagraph"):
+                    subpara_text = f"\t\t{normalize_whitespace(subpara.get_text(separator=' ', strip=True))}\n"
+                    text += subpara_text
+                continue
+            text += f"\t{normalize_whitespace(tag.get_text(separator=' ', strip=True))}\n"
+
         else:
+            if tag.find("paragraph"):
+                content_before, paragraph_text = parseInnerParagraph(tag)
+                text += f"{content_before}\n{paragraph_text}\n"
+                continue
             text += f"{normalize_whitespace(tag.get_text(separator=' ', strip=True))}\n"
     
     return text.strip()
             
 
 
-text = """<section id="ID_1438402520145_001" name="s13" reason="inEffect" startPeriod="2020-11-29" status="operational" temporalId="s13"><num value="13">13.</num><heading>Citation of Ordinance</heading><subsection id="ID_1438410782962_004" name="1" temporalId="s13_1"><num value="1">(1)</num><leadIn>Where any Ordinance is referred to, it shall be sufficient for all purposes to
-                cite such Ordinance by—</leadIn><paragraph id="ID_1438410782962_012" name="a" temporalId="s13_1_a"><num value="a">(a)</num><content>the title, short title or citation thereof;</content></paragraph><paragraph id="ID_1438410782962_020" name="b" temporalId="s13_1_b"><num value="b">(b)</num><content>its number among the Ordinances of the year in which it was enacted;
-                    or</content></paragraph><paragraph id="ID_1438410782977_005" name="c" temporalId="s13_1_c"><num value="c">(c)</num><leadIn>any chapter number lawfully given to it under the authority of—</leadIn><subparagraph id="ID_1483429737793_001" name="1" temporalId="s13_1_c_1"><num value="1">(i)</num><content>the Legislation Publication Ordinance
-                        (Cap.
-                        614);
-                        or</content></subparagraph><subparagraph id="ID_1483429737808_002" name="2" temporalId="s13_1_c_2"><num value="2">(ii)</num><content>any other Ordinance providing for the issue of a revised or other
-                        edition of the laws of Hong Kong. <sourceNote>(Replaced <ref href="hk/2011/13">13 of 2011 s. 29</ref>)</sourceNote></content></subparagraph></paragraph></subsection><subsection id="ID_1438410782977_012" name="2" temporalId="s13_2"><num value="2">(2)</num><leadIn>Any reference made to any Ordinance, in accordance with the provisions of
-                subsection (1), may be made according to the title, short title, citation, number or
-                chapter number used
-                in—
-                    <sourceNote>(Amended <ref href="hk/2020/21">21 of 2020 s. 7</ref>)</sourceNote></leadIn><paragraph id="ID_1604369753523_001" name="a" temporalId="s13_2_a"><num value="a">(a)</num><content>copies of Ordinances printed by the Government Printer; or</content></paragraph><paragraph id="ID_1604369753523_002" name="b" temporalId="s13_2_b"><num value="b">(b)</num><content>verified copies of Ordinances. <sourceNote>(Amended <ref href="hk/2020/21">21 of 2020 s. 7</ref>)</sourceNote></content></paragraph></subsection><subsection id="ID_1604369753523_003" name="3" temporalId="s13_3"><num value="3">(3)</num><leadIn>In this section— </leadIn><def name="VerifiedCopies"><term>verified copies</term> (<term xml:lang="zh-Hant-HK">經核證文本</term>) means
-                verified copies within the meaning of <ref>section 5(1)</ref> of the Legislation
-                Publication Ordinance (<ref href="/hk/cap614">Cap. 614</ref>). <sourceNote>(Added <ref href="hk/2020/21">21 of 2020 s. 7</ref>)</sourceNote></def></subsection><sourceNote>(Amended <ref href="/hk/1974/ln57">L.N. 57 of 1974</ref>)</sourceNote></section>"""
+text = """<schedule id="ID_1438402599206_001" name="sch0" reason="inEffect" startPeriod="2020-06-18" status="operational" temporalId="sch0">
+   <num value="0">
+    Schedule
+   </num>
+   <note class="align_right" role="crossReferences" type="inline">
+    [
+    <ref href="/hk/cap112V/s2">
+     s.
+                2
+    </ref>
+    ]
+    <marker role="blank-line"/>
+    <marker role="blank-line"/>
+   </note>
+   <heading>
+    Article
+            9 of the Agreement between the Government of the Hong Kong Special Administrative Region
+            of the People’s Republic of China and the Government of the State of Israel Concerning
+            Air Services
+    <marker role="blank-line"/>
+    <marker role="blank-line"/>
+   </heading>
+   Done, in
+        duplicate, at Hong Kong on the 19th day of March 1998 in the English and Hebrew
+            languages.
+   <crossHeading>
+   </crossHeading>
+   <text class="align_center">
+    “
+    <b>
+     ARTICLE
+                9
+    </b>
+    <marker role="blank-line"/>
+    <marker role="blank-line"/>
+    <b>
+     Avoidance of Double
+                Taxation
+    </b>
+    <marker role="blank-line"/>
+    <marker role="blank-line"/>
+   </text>
+   <subsection class="left_indent_18 first_line_indent_0" id="ID_1586835421656_001" name="1" temporalId="sch0_1">
+    <num class="align_left" value="1">
+     (1)
+    </num>
+    <content>
+     Income or profits derived from the operation of aircraft in international
+                traffic by an airline of one Contracting Party, including participation in a pool
+                service, a joint air transport operation or an international operating agency, which
+                are subject to tax in the area of that Contracting Party shall be exempt from income
+                tax, profits tax and all other taxes on income or profits imposed in the area of the
+                other Contracting Party.
+    </content>
+   </subsection>
+   <subsection class="left_indent_18 first_line_indent_0" id="ID_1586835421656_002" name="2" temporalId="sch0_2">
+    <num class="align_left" value="2">
+     (2)
+    </num>
+    <content>
+     Capital and assets of an airline of one Contracting Party relating to the
+                operation of aircraft in international traffic shall be exempt from taxes of every
+                kind and description on capital and assets imposed in the area of the other
+                Contracting Party.
+     <marker role="section-break"/>
+    </content>
+   </subsection>
+   <subsection class="left_indent_18 first_line_indent_0" id="ID_1586835421656_003" name="3" temporalId="sch0_3">
+    <num class="align_left" value="3">
+     (3)
+    </num>
+    <content>
+     Gains from the alienation of aircraft operated in international traffic and
+                movable property pertaining to the operation of such aircraft which are received by
+                an airline of one Contracting Party shall be exempt from any tax on gains imposed in
+                the area of the other Contracting Party.
+    </content>
+   </subsection>
+   <subsection class="left_indent_18 first_line_indent_0" id="ID_1586835421656_004" name="4" temporalId="sch0_4">
+    <num class="align_left" value="4">
+     (4)
+    </num>
+    <leadIn>
+     For the purposes of this Article:
+    </leadIn>
+   </subsection>
+   <subsection id="ID_1586835421656_005" name="0" temporalId="sch0_0">
+    <paragraph id="ID_1586835421656_006" name="a" temporalId="sch0_0_a">
+     <num value="a">
+      (a)
+     </num>
+     <def name="IncomeOrProfits">
+      <content>
+       the term “
+       <term class="old_format">
+        income or profits
+       </term>
+       ” includes
+                        revenues and gross receipts from the operation of aircraft for the carriage
+                        of persons, livestock, goods, mail or merchandise in international traffic
+                        including:
+      </content>
+     </def>
+     <subparagraph id="ID_1586835421656_007" name="i" role="subSubParagraph" temporalId="sch0_0_a_i">
+      <num value="i">
+       (i)
+      </num>
+      <content>
+       the charter or rental of aircraft;
+      </content>
+     </subparagraph>
+     <subparagraph id="ID_1586835421656_008" name="ii" role="subSubParagraph" temporalId="sch0_0_a_ii">
+      <num value="ii">
+       (ii)
+      </num>
+      <content>
+       the sale of tickets or similar documents, and the provision of services
+                        connected with such carriage, either for the airline itself or for any other
+                        airline; and
+      </content>
+     </subparagraph>
+     <subparagraph id="ID_1586835421656_009" name="iii" role="subSubParagraph" temporalId="sch0_0_a_iii">
+      <num value="iii">
+       (iii)
+      </num>
+      <content>
+       interest on funds directly connected with the operation of aircraft in
+                        international traffic;
+      </content>
+     </subparagraph>
+    </paragraph>
+    <paragraph id="ID_1586835421656_010" name="b" temporalId="sch0_0_b">
+     <num value="b">
+      (b)
+     </num>
+     <def name="InternationalTraffic">
+      the term “
+      <term class="old_format">
+       international
+                        traffic
+      </term>
+      ” means any carriage by an aircraft except when such carriage
+                    is solely between places in the area of the other Contracting Party;
+     </def>
+    </paragraph>
+    <paragraph id="ID_1586835421656_011" name="c" temporalId="sch0_0_c">
+     <num value="c">
+      (c)
+     </num>
+     <def name="AirlineOfOneContractingParty">
+      the term “
+      <term class="old_format">
+       airline
+                        of one Contracting Party
+      </term>
+      ” means, in the case of the Hong Kong Special
+                    Administrative Region, an airline incorporated and having its principal place of
+                    business in the Hong Kong Special Administrative Region and, in the case of the
+                    State of Israel, an airline substantially owned and effectively controlled by
+                    the Government of the State of Israel or its
+                    nationals;
+      <marker role="section-break"/>
+     </def>
+    </paragraph>
+    <paragraph id="ID_1586835421656_012" name="d" temporalId="sch0_0_d">
+     <num value="d">
+      (d)
+     </num>
+     <def name="CompetentAuthority">
+      the term “
+      <term class="old_format">
+       competent
+                        authority
+      </term>
+      ” means, in the case of the Hong Kong Special Administrative
+                    Region, the Commissioner of Inland Revenue or his authorised representative, or
+                    any person or body authorised to perform any functions at present exercisable by
+                    the Commissioner or similar functions, and, in the case of the State of Israel,
+                    the State Revenue Administration, Ministry of Finance, or their authorised
+                    representative.
+     </def>
+    </paragraph>
+   </subsection>
+   <subsection class="left_indent_18 first_line_indent_0" id="ID_1586835421656_013" name="5" temporalId="sch0_5">
+    <num class="align_left" value="5">
+     (5)
+    </num>
+    <content>
+     The competent authorities of the Contracting Parties shall, through
+                consultation, endeavour to resolve by mutual agreement any disputes regarding the
+                interpretation or application of this Article. Article 18 (Settlement of Disputes)
+                shall not apply to any such dispute.
+    </content>
+   </subsection>
+   <subsection class="left_indent_18 first_line_indent_0" id="ID_1586835421656_014" name="6" temporalId="sch0_6">
+    <num class="align_left" value="6">
+     (6)
+    </num>
+    <leadIn>
+     Notwithstanding Article 22 (Entry into Force) each Contracting Party shall
+                notify to the other the completion of the procedures required by its law for the
+                bringing into force of this Article and the Article shall thereupon have
+                effect:
+    </leadIn>
+   </subsection>
+   <subsection id="ID_1586835421671_015" name="0" temporalId="sch0_0">
+    <paragraph id="ID_1586835421671_016" name="a" temporalId="sch0_0_a">
+     <num value="a">
+      (a)
+     </num>
+     <content>
+      in the Hong Kong Special Administrative Region, for any year of assessment
+                    beginning on or after 1st April in the calendar year next following that in
+                    which this Agreement enters into force;
+     </content>
+    </paragraph>
+    <paragraph id="ID_1586835421671_017" name="b" temporalId="sch0_0_b">
+     <num value="b">
+      (b)
+     </num>
+     <content>
+      in the State of Israel, for any year of assessment beginning on or after
+                    1st January in the calendar year next following that in which this Agreement
+                    enters into force.
+     </content>
+    </paragraph>
+   </subsection>
+   <subsection class="left_indent_18 first_line_indent_0" id="ID_1586835421671_018" name="7" temporalId="sch0_7">
+    <num class="align_left" value="7">
+     (7)
+    </num>
+    <leadIn>
+     Notwithstanding Article 20 (Termination) where notice of termination of this
+                Agreement is given under that Article, this Article shall cease to have
+                effect:
+     <marker role="section-break"/>
+    </leadIn>
+   </subsection>
+   <subsection id="ID_1586835421671_019" name="0" temporalId="sch0_0">
+    <paragraph id="ID_1586835421671_020" name="a" temporalId="sch0_0_a">
+     <num value="a">
+      (a)
+     </num>
+     <content>
+      in the Hong Kong Special Administrative Region, for any year of assessment
+                    beginning on or after 1st April in the calendar year next following that in
+                    which notice is given;
+     </content>
+    </paragraph>
+    <paragraph id="ID_1586835421671_021" name="b" temporalId="sch0_0_b">
+     <num value="b">
+      (b)
+     </num>
+     <content>
+      in the State of Israel, for any year of assessment beginning on or after
+                    1st January in the calendar year next following that in which notice is
+                    given.
+     </content>
+    </paragraph>
+   </subsection>
+   <subsection class="left_indent_18 first_line_indent_0" id="ID_1586835421671_022" name="8" temporalId="sch0_8">
+    <num class="align_left" value="8">
+     (8)
+    </num>
+    <content>
+     This Article shall cease to have effect in the event that an agreement for the
+                avoidance of double taxation with respect to taxes on income, providing for similar
+                exemptions to those in this Article, enters into force between the Contracting
+                Parties.”.
+    </content>
+   </subsection>
+  </schedule>"""
 
-soup = bs4.BeautifulSoup(text, "xml")
-print(section2md(soup.find("section")))
+if __name__ == "__main__":
+    soup = bs4.BeautifulSoup(text, "xml")
+    print(section2md(soup.find("schedule")))
